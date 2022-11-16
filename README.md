bc84ba32daeb4c2f58216230ece0f9303cba8276
david avni <davidavni1221@mail.com>
2022-11-16 17:30:54 +0200

    16/11

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+instruction for load app
 
+in the backend- npm i, npm run dev
+in the frontend- npm i, npm run dev
--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use(session)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, 'public')))
+  app.use(express.static('public'));
 } else {
   const corsOptions = {
     origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:8081', 'http://localhost:8081', 'http://127.0.0.1:5173', 'http://localhost:5173'],
@@ -51,7 +51,18 @@ app.get('/**', (req, res) => {
 })
 
 const logger = require('./services/logger.service')
-const port = process.env.PORT || 3030
-http.listen(port, () => {
-  logger.info('Server is running on port: ' + port)
-})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+console.log(`App listening on port ${port}!`)
+});
+
+// const port = process.env.PORT || 3030
+// http.listen(port, () => {
+//   logger.info('Server is running on port: ' + port)
+// })
+
+// "start": "set NODE_ENV=development&&nodemon server.js",
+
+// app.use(express.static(path.resolve(__dirname, 'public')))
